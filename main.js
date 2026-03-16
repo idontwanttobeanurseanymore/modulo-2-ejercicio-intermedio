45fee1fbdb21de42bb2b69fd3ed193dc642213d3
idontwanttobeanurseanymore <martaariasolmo@hotmail.com>
2026-03-16 20:52:08 +0100

    FUNCIONA

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const playBtn = document.querySelector(".js_playBtn");
 const replayBtn = document.querySelector(".js_replayBtn")
 
 //DATA
-let playerPlay = playerChoice.value
+
 let playerResult = document.querySelector(".js_player_result");
 let cpuResult = document.querySelector(".js_CPU_result");
 let messageResult = document.querySelector(".js_message_result");
@@ -13,53 +13,53 @@ let messageResult = document.querySelector(".js_message_result");
 //FUNCTIONS
 function randomChoice(number){
     number = Math.floor(Math.random()*10);
+    let cpuPlay = cpuResult.value
     if (number <= 3){
         //0, 1, 2, 3
-        cpuResult = "rock"
+        cpuPlay = "rock"
     }else if (number <=6){
         //4, 5, 6
-        cpuResult = "paper"
+        cpuPlay = "paper"
     }else if(number <= 9){
         //7, 8, 9
-        cpuResult = "scissors"
+        cpuPlay = "scissors"
     }
+    return cpuPlay
 }
-function playResults(playerPlay, cpuResult){
-    randomChoice();
-    if(playerPlay = cpuResult){
-    messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuResult}, así que... ¡Empatamos!`
+function playResults(){
+    const playerPlay = playerChoice.value;
+    const cpuPlay = randomChoice();
+    playerResult.innerHTML = playerPlay;
+    cpuResult.innerHTML = cpuPlay;
+    if(playerPlay === cpuPlay){
+    messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuPlay}, así que... ¡Empatamos!`
     }else{
-        if(playerPlay = "rock"){
-            if(cpuResult = "paper"){
-                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuResult}, así que...¡Yo gano!`
-            }else if(cpuResult = "scissors"){
-                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuResult}, así que...¡Tú ganas!`
+        if(playerPlay === "rock"){
+            if(cpuPlay === "paper"){
+                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuPlay}, así que...¡Yo gano!`
+            }else if(cpuPlay === "scissors"){
+                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuPlay}, así que...¡Tú ganas!`
             }
-        }if(playerPlay = "paper"){
-            if(cpuResult = "scissors"){
-                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuResult}, así que...¡Yo gano!`;
-            }else if(cpuResult = "rock"){
-                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuResult}, así que...¡Tú ganas!`;
+        }if(playerPlay === "paper"){
+            if(cpuPlay === "scissors"){
+                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuPlay}, así que...¡Yo gano!`;
+            }else if(cpuPlay === "rock"){
+                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuPlay}, así que...¡Tú ganas!`;
             }
-        }if(playerPlay = "scissors"){
-            if(cpuResult = "rock"){
-                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuResult}, así que...¡Yo gano!`
-            }else if(cpuResult = "paper"){
-                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuResult}, así que...¡Tú ganas!`
+        }if(playerPlay === "scissors"){
+            if(cpuPlay === "rock"){
+                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuPlay}, así que...¡Yo gano!`
+            }else if(cpuPlay === "paper"){
+                messageResult.innerHTML = `Elegiste ${playerPlay} y yo ${cpuPlay}, así que...¡Tú ganas!`
             }
         }
     }
 }
-//EVENTS' FUNCTIONS
-const clickPlayBtn = () => {
-    playResults()
-}
-//eventos
-clickPlayBtn.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    
-})
-    
+playBtn.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  playResults();
+});
+
 /*
 replayBtn.addEventListener('click', () => {
     variable.classList.contains(.collapsed) +add/remove       
